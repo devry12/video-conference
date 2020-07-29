115b68f056d2c6959fa01a7a53da66481ddb7dd2
devry12 <devrykawiryan@gmail.com>
2020-07-29 18:19:51 +0700

    first commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
 const app = express();
 const server = require('http').createServer(app)
 const io = require('socket.io')(server)
